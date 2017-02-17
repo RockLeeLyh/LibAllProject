810588a934ae1432a9e3b367ea020188e9dad11a
rocklee <445130792@qq.com>
2017-02-17 12:28:03 +0800

    导入项目

---
+++ AppDemo/src/main/java/com/rock/lee/my/all/project/lib/MainActivity.java
@@ -0,0 +1,12 @@
+package com.rock.lee.my.all.project.lib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
